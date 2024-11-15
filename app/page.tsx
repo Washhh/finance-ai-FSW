1572d37ffd671b774492dc7089e36f677c24c4f4
Washington Igor <washington.igor22@gmail.com>
2024-11-15 18:32:34 -0300

    feat: ajustando configuração das rotas do clerk para auto redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import Navbar from "./_components/navbar";
 
 export default function Home() {
-  const { userId } = auth();
-  if (!userId) {
-    redirect("/login");
-  }
   return <Navbar />;
 }
